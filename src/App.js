bae77414b6c273c859e5cd6e753d0b068d9f021e
KerenBermeo <bermeokeren@gmail.com>
2024-03-21 22:23:18 -0500

    new commit kbdev

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
-import { AluminumWindows } from './components/AluminumWindows';
-import { BathroomPartitions } from './components/BathroomPartitions';
-import ContactInformation from './components/ContactInformation';
+//import { AluminumWindows } from './components/AluminumWindows';
+//import { BathroomPartitions } from './components/BathroomPartitions';
+//import ContactInformation from './components/ContactInformation';
 import { OurProducts } from './components/OurProducts';
-import { OurServices } from './components/OurServices';
-import { StarBar } from './components/StarBar';
-import { SteelHandrail } from './components/SteelHandrail';
+//import { OurServices } from './components/OurServices';
+//import { StarBar } from './components/StarBar';
+//import { SteelHandrail } from './components/SteelHandrail';
+
+
 function App() {
   return (
     <>
-      <StarBar/>
+      
       <OurProducts/>
-      <BathroomPartitions/> 
-      <SteelHandrail/>
-      <AluminumWindows/>
-      <OurServices/>
-      <ContactInformation/>
+     
     </>
   );
 }
--- src/components/OurProducts.js
+++ src/components/OurProducts.js
@@ -1,14 +1,54 @@
-import React from 'react'
-import homeImage from '../image/home-img.png';
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 export const OurProducts = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <section className="home" id="home">
-        <h3>Nuestros productos</h3>
+    <Slider {...settings}>
+      <div className="slide">1</div>
+      <div className="slide">2</div>
+      <div className="slide">3</div>
+      <div className="slide">4</div>
+      {/* Agrega más diapositivas según sea necesario */}
+    </Slider>
+  );
+  
+};
 
-        <a href="#divisiones"><img src={homeImage} alt="" /></a>
 
-        <a href="#divisiones" class="btn"> Ver Productos</a>
-    </section>
-  )
-}
